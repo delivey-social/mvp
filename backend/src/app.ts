124ed2d8747dd4cbe6354580113aa1b0fc6cba8f
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-03-29 14:52:19 -0300

    adds connection to database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,23 @@
 import express from "express";
 import route from "./routes/orders";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL!);
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Connected to database");
+});
+db.on("error", (error) => {
+  console.error("Error connecting to database", error);
+});
 
 app.get("/", (req, res) => {
   res.send("Service is online");
