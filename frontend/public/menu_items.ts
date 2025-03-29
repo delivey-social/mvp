d9a851cb7391667754760ebb6e08e647618e4810
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-03-29 16:24:03 -0300

    adds salty menu in home

---
+++ frontend/public/menu_items.ts
@@ -0,0 +1,58 @@
+export const menu_salgados = [
+  {
+    name: "Crepe Francês vegetariano",
+    description:
+      "Queijo muçarela, cebola caramelizada, gorgonzola, nozes e manjericão.",
+    imageUrl: "/salgados/vegetariano.avif",
+    price: 31.9,
+  },
+  {
+    name: "Crepe Bacon",
+    description: "Cubos de bacon, milho e muçarela.",
+    imageUrl: "/salgados/bacon.avif",
+    price: 29.9,
+  },
+  {
+    name: "Crepe Calabresa",
+    description: "Cubos de calabresa e queijo muçarela.",
+    imageUrl: "/salgados/calabresa.avif",
+    price: 27.9,
+  },
+  {
+    name: "Crepe Carne Desfiada",
+    description:
+      "Carne desfiada, cebola caramelizada, cheddar cremoso e muçarela.",
+    imageUrl: "/salgados/carne_desfiada.avif",
+    price: 32.9,
+  },
+  {
+    name: "Crepe Frango Cremoso",
+    description: `Frango cremoso com milho, batata palha, e queijo muçarela. Não é possível retirar o milho deste ítem.`,
+    imageUrl: "/salgados/frango_cremoso.avif",
+    price: 29.9,
+  },
+  {
+    name: "Crepe Frango com Requeijão",
+    description: "Queijo muçarela, frango desfiado e requeijão cremoso.",
+    imageUrl: "/salgados/frango_requeijao.avif",
+    price: 30.9,
+  },
+  {
+    name: "Crepe Marguerita",
+    description: "Cubos de tomate, muçarela, manjericão.",
+    imageUrl: "/salgados/marguerita.avif",
+    price: 23.9,
+  },
+  {
+    name: "Crepe Pizza",
+    description: "Cubos de tomate, muçarela, orégano e presunto.",
+    imageUrl: "/salgados/pizza.jpg",
+    price: 24.9,
+  },
+  {
+    name: "Crepe Rúcula com Tomate seco",
+    description: "Folhas de rúcula, tomate seco e queijo muçarela.",
+    imageUrl: "/salgados/rucula_tomate_seco.jpg",
+    price: 29.9,
+  },
+];
--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -1,3 +1,48 @@
+import { menu_salgados } from "../../../public/menu_items";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex flex-col gap-4 h-full min-h-dvh w-full min-w-dvw">
+      <Navbar />
+
+      <main className="px-10 flex gap-6 flex-col pb-20">
+        <h2 className="font-bold text-2xl mt-4">Santo Crepe</h2>
+
+        <h3 className="font-bold">Salgados</h3>
+
+        {menu_salgados.map((item) => (
+          <MenuItem key={item.name} {...item} />
+        ))}
+      </main>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="text-white top-0 w-full py-6 rounded-b-2xl bg-red-800 border-b-1 flex items-center justify-center font-bold text-xl">
+      Delivery
+    </div>
+  );
+}
+
+interface MenuItemProps {
+  name: string;
+  imageUrl: string;
+  price: number;
+  description: string;
+}
+
+function MenuItem({ name, imageUrl, price, description }: MenuItemProps) {
+  return (
+    <div className="flex justify-between border-[1px] p-4 border-gray-300">
+      <div className="flex flex-col gap-3 w-full max-w-60">
+        <h4 className="font-semibold">{name}</h4>
+        <div className="text-xs text-gray-500">{description}</div>
+
+        <div>R$ {price}</div>
+      </div>
+      <img src={imageUrl} className="w-32" />
+    </div>
+  );
 }
