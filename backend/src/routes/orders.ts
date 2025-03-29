6447486fe314fa5c575f560c9d4c30b9c791e696
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-03-29 15:30:49 -0300

    adds delivered working request

--- backend/src/routes/orders.ts
+++ backend/src/routes/orders.ts
@@ -106,4 +106,32 @@ route.patch("/:id/ready_for_delivery", async (req, res) => {
   }
 });
 
+route.patch("/:id/delivered", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json("A valid order id is required");
+    return;
+  }
+
+  const order = await OrderModel.findById(id).select("status");
+
+  if (!order || order.status !== "READY_FOR_DELIVERY") {
+    res.status(400).json("Order not found or already prepared");
+    return;
+  }
+
+  try {
+    await order.updateOne({ status: "DELIVERED" });
+
+    res.send("Order is delivered");
+    return;
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json("Error updating order status");
+    return;
+  }
+});
+
 export default route;
