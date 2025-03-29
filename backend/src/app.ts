b7bf59d9d03f4fb53f58775e79ba0e8d393cddea
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-03-29 10:35:50 -0300

    adds express and initial server with heartbear request

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Service is online");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.clear();
+  console.log(`Server listening on port ${port}`);
+});
