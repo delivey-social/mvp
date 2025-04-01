5f76206f9b7478ce7a71258a7d394575384c5ff0
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-03-31 23:21:09 -0300

    adds GET and PUT /open requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import sendgrid from "@sendgrid/mail";
 import cors, { CorsOptions } from "cors";
+import ConfigModel from "../models/ConfigModel";
+import { z } from "zod";
 
 dotenv.config();
 
@@ -29,6 +31,51 @@ db.on("error", (error) => {
 
 app.get("/", (req, res) => {
   res.send("Service is online");
+});
+
+app.get("/open", async (req, res) => {
+  try {
+    const config = await ConfigModel.findById("systemConfig");
+
+    if (!config) {
+      res.status(404).send("Config not found");
+      return;
+    }
+
+    res.json({ isOpen: config.isOpen });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+});
+
+const openSchema = z.object({
+  isOpen: z.boolean(),
+});
+
+app.put("/open", async (req, res) => {
+  const { data, error } = openSchema.safeParse(req.body);
+
+  if (error) {
+    console.error(error);
+    res.status(400).send("Invalid request body");
+    return;
+  }
+
+  try {
+    const config = await ConfigModel.findByIdAndUpdate(
+      "systemConfig",
+      { isOpen: data.isOpen },
+      { new: true, upsert: true }
+    );
+
+    await config?.save();
+
+    res.send("Config updated successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.use("/orders", route);
