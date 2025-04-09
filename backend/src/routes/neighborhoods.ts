0d88be860e597934027e1d0e6748911ca21907d3
Thiago Tolotti <thiago.tolotti@riole.com.br>
2025-04-09 15:28:05 -0300

    updates /neighborhoods route

--- backend/src/routes/neighborhoods.ts
+++ backend/src/routes/neighborhoods.ts
@@ -1,22 +1,20 @@
 import express from "express";
+
 import NeighborhoodModel from "../models/NeighborhoodModel";
+import handleError from "../utils/handleError";
 
 const route = express.Router();
 
-route.get("/", async (req, res) => {
-  try {
-    const neighborhoods = await NeighborhoodModel.find({})
-      .collation({ locale: "pt", strength: 1 })
-      .sort({
-        name: "asc",
-      });
+route.get("/", async (_, res) => {
+  const [error, neighborhoods] = await handleError(NeighborhoodModel.find({}));
 
-    res.status(200).json(neighborhoods);
-  } catch (error) {
+  if (error) {
     console.error(error);
     res.status(500).send("Internal server error");
     return;
   }
+
+  res.status(200).json(neighborhoods);
 });
 
 export default route;
