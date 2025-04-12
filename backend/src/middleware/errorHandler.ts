c14c943e0e0898c4792cd3f585f3798b7be32263
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-04-12 11:19:56 -0300

    adds eslint and fixes eslint errors

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,11 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
-export default function errorHandler(
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export default function errorHandler(err: Error, _: Request, res: Response) {
   console.error("Unhandled Error: ", err);
 
   res.status(500).send("Internal Server Error");
--- backend/src/utils/renderEmailFactory.ts
+++ backend/src/utils/renderEmailFactory.ts
@@ -1,7 +1,9 @@
 import { render } from "@react-email/render";
 
-export default function renderEmailFactory(email: (props: any) => any) {
-  return async (props: unknown): Promise<string> => {
+export default function renderEmailFactory<T>(
+  email: (props: T) => React.ReactElement
+) {
+  return async (props: T): Promise<string> => {
     return await render(email(props));
   };
 }
