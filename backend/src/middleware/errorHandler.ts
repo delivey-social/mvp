342200a5de5fc3beb848862c0fb7718d2b00df1c
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2025-04-13 11:31:58 -0300

    improves errorHandler middleware, adds express-async-errors lib

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import HTTPError from "../errors/HTTPError";
 
-export default function errorHandler(
+export default async function errorHandler(
   err: Error,
   _: Request,
   res: Response,
   //eslint-disable-next-line
   __: NextFunction
 ) {
-  console.error("Unhandled Error: ", err);
+  if (err instanceof HTTPError) {
+    res.status(err.statusCode).send(err.message);
+    return;
+  }
 
+  console.error("Unhandled Error: ", err);
   res.status(500).send("Internal Server Error");
 }
