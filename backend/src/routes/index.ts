5e33c768706c975132ddac0e908c64eaf3b929ec
Thiago Tolotti <thiagotolotti@gmail.com>
2025-04-20 19:17:28 -0300

    express serves static images

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,6 +6,8 @@ import neighborhoodsRoute from "./neighborhoods";
 import openRoute from "./open";
 import menuItemsRoute from "./menu-items";
 
+import express from "express";
+
 app.get("/", (_, res) => {
   res.send("Service is online");
 });
@@ -14,5 +16,7 @@ app.use("/orders", ordersRoute);
 app.use("/neighborhoods", neighborhoodsRoute);
 app.use("/open", openRoute);
 app.use("/menu-items", menuItemsRoute);
+
+app.use("/images", express.static("public/menu_images"));
 
 app.use(errorHandler);
