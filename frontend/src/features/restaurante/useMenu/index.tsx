30033735747562d99bf391f948975999a03fbe6d
Thiago Tolotti <thiagotolotti@gmail.com>
2026-04-15 23:54:07 -0300

    fetches menu from backend

--- frontend/src/features/restaurante/useMenu/index.tsx
+++ frontend/src/features/restaurante/useMenu/index.tsx
@@ -1,15 +1,40 @@
 import { IMenuItem } from "@/MenuItems";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface MenuItemDTO {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  category: string;
+  restaurantId: string;
+}
 
 export default function useMenu(): Record<string, IMenuItem[]> {
-  return {
-    Salgados: [
-      {
-        id: "1",
-        name: "Coxinha",
-        description: "Deliciosa coxinha de frango com catupiry",
-        price: 5.0,
-        imageUrl: "https://example.com/coxinha.jpg",
-      },
-    ],
-  };
+  const [data, setData] = useState<Record<string, IMenuItem[]>>({});
+
+  useEffect(() => {
+    axios
+      .get<MenuItemDTO[]>(`${import.meta.env.VITE_BACKEND_URL}/menu-items`)
+      .then((data) => {
+        const transformedData: Record<string, IMenuItem[]> = {};
+        data.data.forEach((item) => {
+          if (!transformedData[item.category]) {
+            transformedData[item.category] = [];
+          }
+          transformedData[item.category].push({
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price,
+            imageUrl: item.imageUrl,
+          });
+        });
+        setData(transformedData);
+      });
+  }, []);
+
+  return data;
 }
