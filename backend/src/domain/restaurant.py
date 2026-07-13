4641ce569b8786ad2b47685c5cef7601b0d324cb
Thiago Tolotti <thiagotolott@gmail.com>
2026-07-13 17:23:16 -0300

    adds restaurant domain concepts

---
+++ backend/src/domain/restaurant.py
@@ -0,0 +1,23 @@
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field
+
+
+class CNPJ(str):
+    pass
+
+
+class MenuItem(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    restaurant_id: UUID
+    name: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=3, max_length=500)
+    price: int = Field(..., gt=0)
+    category: str = Field(..., min_length=3, max_length=50)
+
+
+class Restaurant(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    name: str = Field(..., min_length=3, max_length=100)
+    address: str = Field(..., min_length=5, max_length=200)
+    CNPJ: CNPJ
