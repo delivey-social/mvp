9fe48d0b80118c60d0e1a0b56d4775280761d1e2
Thiago Tolotti <thiagotolott@gmail.com>
2026-07-13 16:39:40 -0300

    adds order entity

---
+++ backend/src/domain/order.py
@@ -0,0 +1,76 @@
+from enum import Enum
+from typing import Self
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field, EmailStr
+from pydantic.config import ConfigDict
+
+
+class PhoneNumberVO(BaseModel):
+    number: str
+
+
+class OrderItem(BaseModel):
+    product_id: UUID
+    price: int = Field(gt=0)
+    quantity: int = Field(gt=0)
+
+
+class OrderUser(BaseModel):
+    email: EmailStr
+    phone: PhoneNumberVO
+    address: str
+
+
+class OrderStatus(Enum):
+    WAITING_PAYMENT = "WAITING_PAYMENT"
+    PREPARING = "PREPARING"
+    READY_FOR_DELIVERY = "READY_FOR_DELIVERY"
+    FINISHED = "FINISHED"
+
+
+class PaymentMethod(Enum):
+    PIX = "PIX"
+    CREDIT_CARD = "CREDIT_CARD"
+    DEBIT_CARD = "DEBIT_CARD"
+
+
+class Order(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    id: UUID = Field(default_factory=uuid4)
+    observation: str = Field(default="")
+    items: list[OrderItem]
+    user: OrderUser
+    status: OrderStatus = Field(default=OrderStatus.WAITING_PAYMENT)
+    payment_method: PaymentMethod
+    items_total: int
+    app_fee: int
+    delivery_fee: int
+
+    @property
+    def total(self) -> int:
+        return self.items_total + self.app_fee + self.delivery_fee
+
+    @classmethod
+    def create(
+        cls,
+        items: list[OrderItem],
+        user: OrderUser,
+        payment_method: PaymentMethod,
+        neighborhood_fee: int,
+        observation: str = "",
+    ) -> Self:
+        items_total = sum(item.price * item.quantity for item in items)
+        app_fee = items_total // 10
+        delivery_fee = neighborhood_fee
+
+        return cls(
+            items=items,
+            user=user,
+            payment_method=payment_method,
+            observation=observation,
+            items_total=items_total,
+            app_fee=app_fee,
+            delivery_fee=delivery_fee,
+        )
