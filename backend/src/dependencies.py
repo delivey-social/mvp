ef3707e8a2e6cf47376849760231e8ab28367aa6
Thiago Tolotti <thiagotolott@gmail.com>
2026-07-17 10:02:39 -0300

    adds types in missing repositories

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,5 +1,9 @@
-from .application.neighborhood import NeighborhoodService
+from .domain.types.repositories.neighborhood import NeighborhoodRepository
+from .domain.types.repositories.restaurant import RestaurantRepository
+from .domain.types.repositories.order import OrderRepository
+
 from .application.order import OrderService
+from .application.neighborhood import NeighborhoodService
 from .application.restaurant import RestaurantService
 
 from .infra.image.disk import DiskImageRepository, ImageRepository
@@ -9,19 +13,18 @@ from .infra.neighborhood.inmemory import InMemoryNeighborhoodRepository
 from .infra.order.inmemory import InMemoryOrderRepository
 from .infra.restaurant.inmemory import InMemoryRestaurantRepository
 
-
 event_bus: EventBus = InMemoryEventBus()
 logger = Logger(event_bus)
 
 image_repo: ImageRepository = DiskImageRepository("data/images")
 
-neighborhood_repo = InMemoryNeighborhoodRepository()
+neighborhood_repo: NeighborhoodRepository = InMemoryNeighborhoodRepository()
 neighborhood_service = NeighborhoodService(neighborhood_repo)
 
-restaurant_repo = InMemoryRestaurantRepository()
+restaurant_repo: RestaurantRepository = InMemoryRestaurantRepository()
 restaurant_service = RestaurantService(restaurant_repo, image_repo)
 
-order_repo = InMemoryOrderRepository()
+order_repo: OrderRepository = InMemoryOrderRepository()
 order_service = OrderService(
     order_repo,
     restaurant_service,
